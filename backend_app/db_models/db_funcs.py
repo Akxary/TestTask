bdcf7a64d2a408b45ec3c139e7feb5c3c3896ea2
pollx <pollxx97@gmail.com>
2024-06-05 21:32:15 +0300

    added initial insert in db logic

--- backend_app/db_models/db_funcs.py
+++ backend_app/db_models/db_funcs.py
@@ -1,12 +1,33 @@
 import pandas as pd
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, DeclarativeBase
+from sqlalchemy import Engine
+from db_models.agrmnt import Agrmnt, BaseRetirement, Payment
 from sqlalchemy import func
 from db_models import agrmnt as db_agrmnt
 from enum import Enum
-from db_connect import engine
+from db_connect import engine, Base
 
 
 def init_db_data(db: Session):
 
     with engine.connect() as connection:
         cur = connection.cursor()
+
+
+def recreate_db_tables():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+
+def insert_agrmnt(db: Session, agr_df: pd.DataFrame) -> int:
+    list_to_write = agr_df.to_dict(orient="records")
+    db.add_all(map(lambda row: Agrmnt(**row), list_to_write))
+    db.commit()
+    return db.query(Agrmnt).count()
+
+
+def insert_base_retirement(db: Session, base_retirement_df: pd.DataFrame) -> int:
+    list_to_write = base_retirement_df.to_dict(orient="records")
+    db.add_all(map(lambda row: BaseRetirement(**row), list_to_write))
+    db.commit()
+    return db.query(BaseRetirement).count()
--- backend_app/routes/start_router.py
+++ backend_app/routes/start_router.py
@@ -11,6 +11,8 @@ async def load_file(file: UploadFile = File(...)):
     file_bytes = await file.read()
     try:
         agr_df, ret_df, _ = get_input_dfs(file_bytes)
+        print(agr_df)
+        print(ret_df)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
     finally:
